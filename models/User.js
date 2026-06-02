f680cb14cd8152000015d5f2d6ce4bd67f057c98
Farhan Abid <farhankhan080304@gmail.com>
2026-06-02 11:14:09 +0530

    fix: import bcryptjs named exports to resolve Turbopack SSR issue

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -14,12 +14,12 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await hash(this.password, 10);
   next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+  return compare(candidatePassword, this.password);
 };
 
 userSchema.methods.toJSON = function () {
